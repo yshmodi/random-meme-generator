1b6508f018b663da220e10e242ec847cf01674d5
Yash Modi <132808293+yshmodi@users.noreply.github.com>
2023-05-07 13:23:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const generateMemeBtn = document.querySelector(".meme-generator .generate-meme-btn");
+const memeImage = document.querySelector(".meme-generator img");
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+const memeAuthor = document.querySelector(".meme-generator .meme-author");
+
+const generateMeme = async () => {
+    const url = 'https://meme-api.com/gimme/wholesomememes';
+    const options = {
+        method: 'GET',
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+
+        memeTitle.textContent = result.title;
+        memeImage.src = result.url;
+        memeAuthor.textContent = "Posted by " + result.author;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
